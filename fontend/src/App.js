7c0eda1fbfa14ab9de0523b31e2023254d807a98
VihKag <nvkhang191@gmail.com>
2023-10-04 15:21:38 +0700

    add fontend

---
+++ fontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="test-tailwind text-red-500">
+          Hello world
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
