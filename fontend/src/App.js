23e021a077eb8248e6bf84648cf78bca54d246b8
PhamQui <20110079@student.hcmute.edu.vn>
2023-10-13 18:51:55 +0700

    gắn Home, chỉnh lại Login, Signup

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,16 +2,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import Login from "./Login";
+import Signup from "./Signup";
 import ProductDetail from './pages/ProductDetail';
+import Home from './pages/Home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProductDetail />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/prodcutdetail" element={<ProductDetail />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
   );
