d19cc0c1d3f382cee687d4d9f008da9be9f1a3b8
VihKag <nvkhang191@gmail.com>
2023-10-17 00:03:28 +0700

    commit file Home.jsx and index.css

--- fontend/src/pages/Home.jsx
+++ fontend/src/pages/Home.jsx
@@ -1,51 +1,75 @@
+import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import React from "react";
+import banner1 from "../assets/banner/banner1.jpg";
 import Carousel from "../components/Carousel/Carousel";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Home = () => {
+  const icontab = [
+    {label: 'Discounts', img: './path'},
+    {label: 'Cashback', img: './path'},
+    {label: 'Payment', img: './path'},
+    {label: 'Payment', img: './path'},
+    {label: 'Payment', img: './path'},
+    {label: 'Payment', img: './path'},
+    {label: 'Payment', img: './path'},
+    {label: 'Payment', img: './path'}
+  ];
   return (
     <>
-      <Header />
-      <Carousel/>
-      <div className="container">
-        <section>
-          {/* Banner quảng cáo */}
-          <div className="banner">
-
-          </div>
-        </section>
-
-        <section>
-          {/* Danh sách Sách Mới */}
-          <h2>Sách Mới</h2>
-          <div className="sach-moi">
-            {/* Hiển thị danh sách sách mới */}
-            <div className="sach">
-              <img src="sach1.jpg" alt="Sách 1" />
-              <p>Tiêu đề sách 1</p>
-              <p>Tác giả sách 1</p>
-              <p>Giá: $19.99</p>
-              <button>Mua ngay</button>
+      <Header />      
+      <div className="container mx-auto">     
+        <div className="homebanner">
+          <Carousel/>
+          <div className="hidden sm:flex mb-5 mt-2">
+            <div className="col-1-4">
+              <img src={banner1} alt="../assets/banner/banner1.jpg" className="rounded-md" />
             </div>
-            {/* Các cuốn sách khác... */}
-          </div>
-        </section>
-
-        <section>
-          {/* Danh sách Sách Bán Chạy */}
-          <h2>Sách Bán Chạy</h2>
-          <div className="sach-ban-chay">
-            {/* Hiển thị danh sách sách bán chạy */}
-            <div className="sach">
-              <img src="sach2.jpg" alt="Sách 2" />
-              <p>Tiêu đề sách 2</p>
-              <p>Tác giả sách 2</p>
-              <p>Giá: $24.99</p>
-              <button>Mua ngay</button>
+            <div className="col-1-4">
+              <img src={banner1} alt="../assets/banner/banner1.jpg" className="rounded-md" />
             </div>
-            {/* Các cuốn sách khác... */}
+            <div className="col-1-4">
+              <img src={banner1} alt="../assets/banner/banner1.jpg" className="rounded-md" />
+            </div>
+            <div className="col-1-4">
+              <img src={banner1} alt="../assets/banner/banner1.jpg" className="rounded-md" />
+            </div>
           </div>
-        </section>
+        </div>  
+
+        <div className="flex justify-between flex-wrap">
+          {icontab.map((item,index)=>(
+            <div key={index} className="w-1/4 md:col-1-8">
+              <div className="text-4xl lg:text-5xl w-full flex justify-center"><FontAwesomeIcon icon="fa-solid fa-receipt" style={{color: "#ca1c1c",}} /></div>
+              <div className="max-w-max mx-auto">{item.label}</div>
+          </div>  
+          ))}      
+        </div>
+
+        <div className="Category-display my-4">
+          <div className="Category-header bg-green-300">
+            <div className="inline-block m-2 text-2xl font-medium">
+            <FontAwesomeIcon icon="fa-solid fa-fire" size="xl" style={{color: "#ff0000",}} />
+              <span className="ml-2">XU HƯỚNG MUA SÁCH</span>
+            </div>
+          </div>
+          <div className="Category-tab">              
+            <ul className="flex flex-wrap text-md font-medium text-center text-black">
+              <li class="mr-5">
+                   <a href="/#" class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 tabhome-btn">Category 1</a>
+              </li>
+              <li className="mr-5">
+                  <a href="/#"  className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 tabhome-btn">Category </a>                </li>
+              <li className="mr-5">
+                  <a href="/#" className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 tabhome-btn">Category 3</a>
+              </li>
+              <li className="mr-5">
+                  <a href="/#" className="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 tabhome-btn">Category 4</a>
+              </li>
+            </ul>
+          </div>
+          <div className="tab-product"></div>            
+        </div>
       </div>
       <Footer />
     </>
